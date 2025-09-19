4358a8e3ea6a18374d2677b31db89b12329f090f
Robson Carolino <robsoncarolino96@gmail.com>
2025-09-19 17:12:57 -0300

    feat: Atualiza os links das redes sociais no rodapé

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,10 @@ const Footer = () => {
       <a href="https://www.instagram.com/leoneldeodoro.pilates/" target="_blank" rel="noopener noreferrer">
         <FaInstagram />
       </a>
-      <a href="https://www.whatsapp.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://api.whatsapp.com/message/PT6ZWBFS3R6DJ1" target="_blank" rel="noopener noreferrer">
         <FaWhatsapp />
       </a>
-      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/leoneldeodoro.pilates" target="_blank" rel="noopener noreferrer">
         <FaFacebook />
       </a>
     </footer>
